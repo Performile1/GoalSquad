c46f7c4f9e45e179adb05da8e638792d431aa33e
Performile <admin@performile.com>
2026-04-16 07:28:18 +0200

    fix: exclude mobile directory from Next.js build

--- next.config.js
+++ next.config.js
@@ -15,6 +15,14 @@ const nextConfig = {
       bodySizeLimit: '2mb',
     },
   },
+  webpack: (config, { isServer }) => {
+    // Ignore mobile directory during build
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/node_modules', '**/mobile/**'],
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
